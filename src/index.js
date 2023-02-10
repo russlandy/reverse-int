ca5b706c409714761b7cedf215ee3ab0cf08b007
russlandy <ruslandabysov@gmail.com>
2023-02-10 15:09:16 +0300

    resolve done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * (-1)
+    }
+  let arr = ('' + n).split('').map(Number);
+  let newArr = arr.reverse();
+  return +newArr.join('');
 }
